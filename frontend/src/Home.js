e2953f070c9793858364789c2027a0361d334caf
Ryan Dunetz <ryandunetz@gmail.com>
2025-10-17 10:46:18 -0400

    Fixed Home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -3,48 +3,44 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-// import SocialsIconRow from './Socials'
 import Divider from '@mui/material/Divider';
-
 
 function Home() {
 
-  const email = ""
+    const PROJECT_NAME = "VTCS Pathfinder"
 
-  return (
-    <Container maxWidth={false} className="Home">
-      <Box className="Home-main">
-        <Typography variant="h2" gutterBottom className="Home-title animate-fade-slide">
-            VTCS Pathfinder
-          {/* Hello I'm <span className="accent">Ryan</span>, */}
-          <br/>
-        </Typography>
+    return (
+        <Container maxWidth={false} className="Home">
+            <Box className="Home-main">
+                <Typography variant="h2" gutterBottom className="Home-title animate-fade-slide">
+                    {PROJECT_NAME}
+                    <br />
+                </Typography>
 
-        <Divider className='animate-fade-slide' />
+                <Divider className='animate-fade-slide' />
 
-        {/* <div className='animate-fade-slide'>
-          <SocialsIconRow email={email} subject='Job Offer' body='Hello Ryan, we want to offer you a job!' />
-        </div> */}
+                <br/>
 
-        <Typography variant="h5" gutterBottom className="Home-subtitle animate-fade-slide">
-          I'm a senior attending Virginia Tech studying Computer Science. My interests
-          are robotics, AI, and coding. This is my portfolio and just a place I can 
-          just develop my skills.
-        </Typography>
+                <Typography variant="h5" gutterBottom className="Home-subtitle animate-fade-slide">
+                    Our senior capstone project, VTCS Pathfinder, is a web application designed to help Virginia Tech Computer Science students plan their academic journey efficiently. It allows users to visualize and organize their semesters based on the official VT CS degree checklist, ensuring that prerequisites and graduation requirements are met. The tool provides an intuitive interface for course selection, semester planning, and progress tracking throughout a student’s academic career.
+                </Typography>
 
-        {/* <Button
-          variant="contained"
-          size="large"
-          className="Home-button"
-          href="/Review"
-        >
-          Add a Review
-        </Button> */}
+                <br/>
 
-        {/* <img src="vtMap.png" alt="VT Map" className="Home-image" /> */}
-      </Box>
-    </Container>
-  );
+                <Button
+                    variant="contained"
+                    size="large"
+                    className="Home-button animate-fade-slide"
+                    href="/auth"
+                >
+                    Get Started
+                </Button>
+
+                {/* <img src="vtMap.png" alt="VT Map" className="Home-image" /> */}
+
+            </Box>
+        </Container>
+    );
 }
 
 export default Home;
